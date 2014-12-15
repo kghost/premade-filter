0f0997762ad936e582127cf5f2808f00fbacf2a5
Eugene Khorev <ek@mindmeal.ru>
2014-12-15 14:51:51 +0300

    Fixed search algorithm

--- PremadeFilter.lua
+++ PremadeFilter.lua
@@ -115,153 +115,6 @@ function LFGListEntryCreation_PopulateGroups(self, dropDown, info)
 	end
 end
 
---[[
-function PremadeFilter_Button_OnClick(self)
-	ToggleDropDownMenu(1, nil, PremadeFilter_Options, PremadeFilter_Button, 0, 0);
-end
-
-function PremadeFilter_Options_OnLoad(self, level)
-	if not level then
-		return
-	end
-	
-	if (level == 1) then
-		local categories = C_LFGList.GetAvailableCategories();
-		
-		for i=1, #categories do
-			local info = UIDropDownMenu_CreateInfo();
-			
-			local categoryID = categories[i];
-			local categoryName, separateRecommended, autoChoose, preferCurrentArea = C_LFGList.GetCategoryInfo(categoryID);
-			
-			local groups = C_LFGList.GetAvailableActivityGroups(categoryID, 0);
-			if next(groups) ~= nil then
-				info.hasArrow = true;
-			else
-				info.hasArrow = false;
-			end
-			
-			info.notCheckable = false;
-			info.text = categoryName;
-			info.func = PremadeFilter_Options_OnClick;
-			info.value = {
-				["category"] = {
-					["ID"] = categoryID;
-					["name"] = categoryName;
-				};
-			}
-			
-			UIDropDownMenu_AddButton(info, 1);
-		end
-	end
-	
-	if (level == 2) then
-		local category = UIDROPDOWNMENU_MENU_VALUE["category"];
-		
-		local groups = C_LFGList.GetAvailableActivityGroups(category["ID"], 0);
-		
-		for i=1, #groups do
-			local info = UIDropDownMenu_CreateInfo();
-			
-			local groupID = groups[i];
-			local groupName = C_LFGList.GetActivityGroupInfo(groupID);
-			
-			local activities = C_LFGList.GetAvailableActivities(browseCategory, groupID, 0, "");
-			if next(groups) ~= nil then
-				info.hasArrow = true;
-			else
-				info.hasArrow = false;
-			end
-			
-			info.notCheckable = false;
-			info.text = groupName;
-			info.func = PremadeFilter_Options_OnClick;
-			info.value = {
-				["category"] = category;
-				["group"] = {
-					["ID"] = groupID;
-					["name"] = groupName;
-				};
-			};
-			
-			UIDropDownMenu_AddButton(info, level);
-		end
-	end
-	
-	if (level == 3) then
-		local category = UIDROPDOWNMENU_MENU_VALUE["category"];
-		local group = UIDROPDOWNMENU_MENU_VALUE["group"];
-		
-		local activities = C_LFGList.GetAvailableActivities(category["ID"], group["ID"], 0, "");
-		
-		for i=1, #activities do
-			local info = UIDropDownMenu_CreateInfo();
-			
-			local activityID = activities[i];
-			local activityName, shortName, categoryID, groupID, itemLevel, filters, minLevel, maxPlayers, displayType = C_LFGList.GetActivityInfo(activityID);
-			
-			info.hasArrow = false;
-			info.notCheckable = false;
-			info.text = shortName;
-			info.func = PremadeFilter_Options_OnClick;
-			info.value = {
-				["category"] = category;
-				["group"] = group;
-				["activity"] = {
-					["ID"] = activityID;
-					["name"] = shortName;
-				};
-			};
-			
-			UIDropDownMenu_AddButton(info, level);
-		end
-	end
-end
-
-function PremadeFilter_Options_OnClick(self, arg1, arg2, checked)
-	local value = self.value;
-	local categoryName = "";
-	
-	if value["category"] then
-		categoryName = value["category"]["name"];
-	end
-	
-	if value["group"] then
-		categoryName = categoryName.." - "..value["group"]["name"];
-	end
-	
-	if value["activity"] then
-		categoryName = categoryName.." - "..value["activity"]["name"];
-	end
-	
-	PremadeFilter_Title:SetText(categoryName);
-end
-
-function PremadeFilter_Options_GetExpansion(categoryID, activityID)
-	local expansions = {
-		[3] = {
-			[9]		= LE_EXPANSION_CLASSIC,
-			[293]	= LE_EXPANSION_CLASSIC,
-			[294]	= LE_EXPANSION_CLASSIC,
-			[295]	= LE_EXPANSION_CLASSIC,
-		}
-	}
-	
-	local category = expansions[categoryID];
-	if category then
-		local expansion = category[activityID];
-		if expansion then
-			return {
-				["expansionID"] = expansion;
-				["expansionName"] = _G["EXPANSION_NAME"..expansion];
-			}
-		end
-	end
-	
-	return false;
-end
-]]--
-
 function LFGListSearchPanel_DoSearch(self)
 	local searchText = "";--self.SearchBox:GetText();
 	C_LFGList.Search(self.categoryID, searchText, self.filters, self.preferredFilters);
@@ -277,13 +130,12 @@ function LFGListSearchPanel_UpdateResultList(self)
 	self.applications = C_LFGList.GetApplications();
 	
 	local searchText = self.SearchBox:GetText():lower();
+	local include = {}
+	local exclude = {}
 	
 	if searchText ~= "" then
 		local words = {}
 		for w in searchText:gmatch("%S+") do table.insert(words, w) end
-		
-		local include = {}
-		local exclude = {}
 		
 		for i,w in pairs(words) do
 			if w:sub(1,1) ~= "-" then
@@ -295,17 +147,20 @@ function LFGListSearchPanel_UpdateResultList(self)
 				end
 			end
 		end
-		
-		local numResults = 0;
-		local newResults = {};
-		for i=1, #self.results do
-			local matches = true;
+	end
+	
+	local numResults = 0;
+	local newResults = {};
+	
+	for i=1, #self.results do
+		local matches = true;
 
-			local id, activityID, name, comment, voiceChat, iLvl, age, numBNetFriends, numCharFriends, numGuildMates, isDelisted = C_LFGList.GetSearchResultInfo(self.results[i]);
-			local activityName = C_LFGList.GetActivityInfo(activityID);
+		local id, activityID, name, comment, voiceChat, iLvl, age, numBNetFriends, numCharFriends, numGuildMates, isDelisted = C_LFGList.GetSearchResultInfo(self.results[i]);
+		local activityName = C_LFGList.GetActivityInfo(activityID);
 
-			name = name:lower();
+		name = name:lower();
 
+		if next(include) ~= nil then
 			for i,w in pairs(include) do
 				local nameMatch = name:find(w);
 				if nameMatch == nil then
@@ -313,26 +168,33 @@ function LFGListSearchPanel_UpdateResultList(self)
 					break 
 				end
 			end
-			
-			if matches then
-				for i,w in pairs(exclude) do
-					local nameMatch = name:find(w);
-					if nameMatch ~= nil then
-						matches = false;
-						break 
-					end
+		end
+		
+		if matches and next(exclude) ~= nil then
+			for i,w in pairs(exclude) do
+				local nameMatch = name:find(w);
+				if nameMatch ~= nil then
+					matches = false;
+					break 
 				end
 			end
-			
-			if matches then
-				numResults = numResults + 1
-				newResults[numResults] = self.results[i];
-			end
 		end
-
-		self.totalResults = numResults;
-		self.results = newResults;
+		
+		-- check additional filters
+		if PremadeFilter_Frame:IsVisible() then
+			--print("ADVANCED SEARCH");
+		else
+			--print("SIMPLE SEARCH");
+		end
+		
+		if matches then
+			numResults = numResults + 1
+			newResults[numResults] = self.results[i];
+		end
 	end
+
+	self.totalResults = numResults;
+	self.results = newResults;
 	
 	LFGListUtil_SortSearchResults(self.results);
 end
@@ -385,7 +247,7 @@ function LFGListSearchPanel_UpdateResults(self)
 	end
 	LFGListSearchPanel_UpdateButtonStatus(self);
 end
-
+--[[
 function PrintTable(t, level)
 	level = level or 0;
 	table.foreach(t, function(k,v)
@@ -397,3 +259,4 @@ function PrintTable(t, level)
 		end
 	end);
 end
+]]--
