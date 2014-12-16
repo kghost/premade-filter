e70b45643295cf77c2b591e6cc087800c71b6c4f
Eugene Khorev <ek@mindmeal.ru>
2014-12-16 19:44:33 +0300

    Fixed advanced PvP filtering (yes, it's back!)

--- PremadeFilter.lua
+++ PremadeFilter.lua
@@ -22,10 +22,14 @@ function PremadeFilter_Frame_OnLoad(self)
 end
 
 function PremadeFilter_OnShow(self)
+	local categoryID = LFGListFrame.categoryID
+	local baseFilters = LFGListFrame.baseFilters;
+	
+	PremadeFilter_Frame.categoryID = categoryID;
+	PremadeFilter_Frame.baseFilters = baseFilters;
+
 	local selectedCategory = LFGListFrame.CategorySelection.selectedCategory;
 	local selectedFilters = LFGListFrame.CategorySelection.selectedFilters;
-	
-	--LFGListFrame_SetBaseFilters(LFGListFrame, LE_LFG_LIST_FILTER_PVP);
 	
 	PremadeFilter_Frame.selectedCategory = selectedCategory;
 	PremadeFilter_Frame.selectedFilters = selectedFilters;
@@ -66,7 +70,24 @@ function PremadeFilter_OnActivitySelected(self, id, buttonType)
 	
 	LFGListEntryCreation_OnActivitySelected(self, id, buttonType);
 end
-
+--[[
+function LFGListEntryCreation_PopulateCategories(self, dropDown, info)
+	print("--LFGListEntryCreation_PopulateCategories");
+	print("self.baseFilters");
+	print(self.baseFilters);
+	local categories = C_LFGList.GetAvailableCategories(self.baseFilters);
+	for i=1, #categories do
+		local categoryID = categories[i];
+		local name, separateRecommended = C_LFGList.GetCategoryInfo(categoryID);
+		if ( separateRecommended ) then
+			LFGListEntryCreation_AddCategoryEntry(self, info, categoryID, name, LE_LFG_LIST_FILTER_RECOMMENDED);
+			LFGListEntryCreation_AddCategoryEntry(self, info, categoryID, name, LE_LFG_LIST_FILTER_NOT_RECOMMENDED);
+		else
+			LFGListEntryCreation_AddCategoryEntry(self, info, categoryID, name, 0);
+		end
+	end
+end
+]]--
 function LFGListEntryCreation_PopulateGroups(self, dropDown, info)
 	if ( not self.selectedCategory ) then
 		--We don't have a category, so we can't fill out groups.
