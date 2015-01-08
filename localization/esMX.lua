51df07e585d9a835bbcaef652b3fa047999fa921
Eugene Khorev <ek@mindmeal.ru>
2015-01-08 06:48:30 +0300

    Bug fixes + optimization + localization

---
+++ localization/esMX.lua
@@ -0,0 +1,14 @@
+local _, L = ...;
+
+if GetLocale() == "esMX" then
+	L["Include words"] = "Palabras para incluir";
+	L["Exclude words"] = "Palabras para excluir";
+	L["At least one word"] = "Por lo menos una palabra";
+	L["The words you want"] = "Palabras deseadas";
+	L["The words you don't want"] = "Palabras no deseadas";
+	L["You want at least one of these words"] = "Quieres por lo menos una de estas palabras";
+
+	L["Monitor new groups in background?"] = "Monitor new groups in background?";
+	
+	L["EXPERIMENTAL:\nWorks only on premades created with Premade Filter addon"] = "EXPERIMENTAL:\nSolo funciona en grupos pre-armados creados por el addon Premade Filter";
+end
